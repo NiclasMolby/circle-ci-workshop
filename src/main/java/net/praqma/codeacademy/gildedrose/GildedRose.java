638ffbda0634bc9a35c70ee4dbe6f6ca49734916
Niclas Mølby <niclas@molby.net>
2019-08-14 10:17:50 +0200

    Fixed errors

--- src/main/java/net/praqma/codeacademy/gildedrose/GildedRose.java
+++ src/main/java/net/praqma/codeacademy/gildedrose/GildedRose.java
@@ -23,14 +23,18 @@ class GildedRose {
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                                items[i].quality = items[i].quality + 2;
                             }
                         }
 
                         if (items[i].sellIn < 6) {
                             if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                                items[i].quality = items[i].quality + 3;
                             }
+                        }
+
+                        if (items[i].sellIn < 1) {
+                            items[i].quality = 0;
                         }
                     }
                 }
